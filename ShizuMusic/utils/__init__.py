f5086e99c6c14387b200442e1ada6b5563f12099
🕊️ ʙᴀᴅ ᴍᴜɴᴅᴀ ❤️ <sukhwinderwarval98@gmail.com>
2026-05-08 12:06:01 +0530

    Update __init__.py

--- ShizuMusic/utils/__init__.py
+++ ShizuMusic/utils/__init__.py
@@ -1,4 +1,11 @@
-# ShizuMusic/utils/__init__.py
+# --------------------------------------------------------------------------------
+#  ShizuMusic © 2026
+#  Developed by Bad Munda ❤️
+#
+#  Unauthorized copying, editing, re-uploading or removing credits
+#  from this source code is strictly prohibited.
+# --------------------------------------------------------------------------------
+
 from ShizuMusic.utils.permissions import is_user_authorized
 
 __all__ = ["is_user_authorized"]
