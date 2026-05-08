e5d1e605a9e0e66e699bd899902f5b14d8cadbac
🕊️ ʙᴀᴅ ᴍᴜɴᴅᴀ ❤️ <sukhwinderwarval98@gmail.com>
2026-05-08 12:05:43 +0530

    Update watcher.py

--- ShizuMusic/core/watcher.py
+++ ShizuMusic/core/watcher.py
@@ -1,6 +1,10 @@
-"""
-ShizuMusic/core/watcher.py
-"""
+# --------------------------------------------------------------------------------
+#  ShizuMusic © 2026
+#  Developed by Bad Munda ❤️
+#
+#  Unauthorized copying, editing, re-uploading or removing credits
+#  from this source code is strictly prohibited.
+# --------------------------------------------------------------------------------
 
 import asyncio
 
