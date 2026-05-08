b5f96c4787bae2daa76c35097bbcb317ba0e494f
🕊️ ʙᴀᴅ ᴍᴜɴᴅᴀ ❤️ <sukhwinderwarval98@gmail.com>
2026-05-08 12:05:08 +0530

    Update player.py

--- ShizuMusic/core/player.py
+++ ShizuMusic/core/player.py
@@ -1,3 +1,11 @@
+# --------------------------------------------------------------------------------
+#  ShizuMusic © 2026
+#  Developed by Bad Munda ❤️
+#
+#  Unauthorized copying, editing, re-uploading or removing credits
+#  from this source code is strictly prohibited.
+# --------------------------------------------------------------------------------
+
 import asyncio
 import time
 
