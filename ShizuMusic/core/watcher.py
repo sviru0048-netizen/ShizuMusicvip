abfd3e69ce749cfc197aaea4f234e8615111d4fc
sviru0048-netizen <sviru0048@gmail.com>
2026-05-11 13:33:26 +0530

    Update copyright and developer information

--- ShizuMusic/core/watcher.py
+++ ShizuMusic/core/watcher.py
@@ -1,6 +1,6 @@
 # --------------------------------------------------------------------------------
-#  ShizuMusic © 2026
-#  Developed by Bad Munda ❤️
+#  KRISH X STAR CODER © 2026
+#  Developed by KRISH X STAR CODER ❤️
 #
 #  Unauthorized copying, editing, re-uploading or removing credits
 #  from this source code is strictly prohibited.
