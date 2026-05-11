70df009f11e8f5bcce000c57b83391f25a03c05e
sviru0048-netizen <sviru0048@gmail.com>
2026-05-11 13:31:39 +0530

    Update copyright and developer information in player.py

--- ShizuMusic/core/player.py
+++ ShizuMusic/core/player.py
@@ -1,10 +1,10 @@
 # --------------------------------------------------------------------------------
-#  ShizuMusic © 2026
-#  Developed by Bad Munda ❤️
+#  KRISH X STAR CODER © 2026
+#  Developed by KRISH X STAR CODER ❤️
 #
 #  Unauthorized copying, editing, re-uploading or removing credits
 #  from this source code is strictly prohibited.
-# --------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------------------------
 
 import asyncio
 import time
